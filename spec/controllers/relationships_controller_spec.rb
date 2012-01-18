61eb7b0690e2d111a51ad8f365e84681a6158b56
Andrew de Laix <delaix@mac.com>
2012-01-18 14:57:20 -0600

    now with AJAX

--- spec/controllers/relationships_controller_spec.rb
+++ spec/controllers/relationships_controller_spec.rb
@@ -25,6 +25,13 @@ describe RelationshipsController do
         response.should be_redirect
       end.should change(Relationship, :count).by(1)
     end
+    
+    it "should create a relationship with AJAX" do
+      lambda do
+        xhr :post, :create, :relationship => { :followed_id => @followed }
+        response.should be_redirect
+      end.should change(Relationship, :count).by(1)
+    end
   end
   
   describe "DELETE 'destroy'" do
@@ -38,6 +45,13 @@ describe RelationshipsController do
     it "should destroy a relationship" do
       lambda do
         delete :destroy, :id => @relationship
+        response.should be_redirect
+      end.should change(Relationship, :count).by(-1)
+    end
+    
+    it "should destroy a relationship wit AJAX" do
+      lambda do
+        xhr :delete, :destroy, :id => @relationship
         response.should be_redirect
       end.should change(Relationship, :count).by(-1)
     end
